7383adb9fb2d2814cd58bdee737e9ac7caddc4a3
Josh Hamann <jhamann@CKT-C02TN1V4HTD6Josh-Hamann.local>
2018-08-23 11:11:50 -0600

    pick me

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # fantasyfootball
 For fantasy football data stuff
+
+# Installation
+Pull Repo Down  
+Verify python 3 and pip installed locally  
+Navigate to fantasyfootball Repo directory (you just created)   
+"pip install -r requirements.txt"  (virutalenv recommended)  
+Get .json key file from repo owner, place in same directory and label "fantasyfootball-definitely-not-a-key.json"  
+
+# How it Works (on a mac)  
+Open Terminal  
+Navigate to fantasyfootball directory  
+"python pick_me.py"  
+Top (x) remaining player's per position output to Terminal, ranked by TeamRank(TO DO: Make better later)  
+
+# Dependencies
+2018 Fantasy Draft gsheet API Access  
+top300.csv  
+team_rank.csv  
---
+++ pick_me.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import pygsheets
+
+top_300_file = 'top300.csv'
+team_rank = 'team_rank.csv'
+service_file = 'fantasyfootball-definitely-not-a-key.json'
+
+def csv_to_df(file_name):
+    """pull top ranked players into df"""
+    df = pd.read_csv(file_name)
+    return df
+
+def gsheet_api_to_df():
+    """Connect to 2018 Fantasy Draft, pull Available Players and return df"""
+    gc = pygsheets.authorize(service_file=service_file)
+    sh = gc.open("2018 Fantasy Draft")
+    wks = sh.worksheet_by_title("AvailablePlayers")
+    cell_matrix = wks.get_all_values(returnas='matrix')
+    df = pd.DataFrame(cell_matrix)
+    
+    #only grab relevant columns
+    df = df.iloc[:,0:5]
+
+    #make header row header and remove that false data row from data set
+    df.columns = df.iloc[0]
+    df = df.iloc[1:]
+    
+    return df
+
+def df_magic(player_rank_df,team_rank_df,player_draft_status_df):
+    """Merge data and output top 3 remaining players of each position, ordered by Team Rank
+    To Do:  Can insert custom ranking mechanism, ML Model, whatever here later"""
+    #join player rank to team rank df
+    player_team_rank_df = pd.merge(player_rank_df,team_rank_df, how='left', on=['Team'])
+    
+    #join resulting df to remaining players
+    mother_load_df = pd.merge(player_team_rank_df,player_draft_status_df,how='left',on=['Player'])
+    #filter by available players
+    mother_load_df = mother_load_df.loc[mother_load_df['Status'] == "Available"]
+    #data cleanup work
+    mother_load_df.rename(columns={'Rank_x': 'Player_Rank', 'Rank_y': 'Team_Rank',
+                                   'Team_x': 'Team', 'Position_x':'Position'}, inplace=True)
+    mother_load_df = mother_load_df.drop(['Rank', 'Team_y', 'Position_y'], axis=1)
+    mother_load_df['Team_Rank'] = mother_load_df['Team_Rank'].fillna(0.0).astype(int)
+    #ranked, grouped by position
+    mother_load_df = mother_load_df.groupby('Position').head(3)
+    
+    return mother_load_df
+
+def main():
+    """Take ESPN top 300, take team ranks(manually loaded), and available players
+    Return a list of choices, choices, choices..."""
+    player_rank_df = csv_to_df(top_300_file)
+    team_rank_df = csv_to_df(team_rank)
+    player_draft_status_df = gsheet_api_to_df()
+    
+    choices_choices = df_magic(player_rank_df,team_rank_df,player_draft_status_df)
+    
+    print (choices_choices)
+    
+if __name__ == "__main__":
+    main()
